cd29d6ce7d94289bcf8585ed76eb0f68570d76c1
coreybrendan <coreybrendan@me.com>
2021-07-08 16:06:53 -0400

    JS written and functioning for all four project screens

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-// find the img with class of .tattoo-screen
-const projectScreen = document.querySelector('.tattoo-screen');
+// screen 1: tattoo tally
+const tattooProject = document.querySelector('.tattoo-screen');
 
-// add event listener
-projectScreen.addEventListener('click', handleClick);
+tattooProject.addEventListener('click', handleTattooClick);
 
-// click event
-function handleClick() {
+function handleTattooClick() {
   console.log('I have been clicked!!');
-  toggleProjectDetails();
+  toggleTattooDetails();
 }
 
-// toggle class name from .hide-details to .show-details
-const toggleProjectDetails = () => {
+const toggleTattooDetails = () => {
   const element = document.getElementsByClassName('hide-details');
   element[0].classList.toggle('show-details');
-  // console.log(element[0].classList.toggle);
 }
 
+// screen 2: bootcamp diaries
+const diariesProject = document.querySelector('.bootcamp-diaries-screen');
 
+diariesProject.addEventListener('click', handleDiariesClick);
 
-// document.querySelector('.tattoo-screen').onClick = function() {
-//   const element = document.getElementsByClassName('hide-details');
-//   element.classList.toggle('show-details');
-// }
+function handleDiariesClick() {
+  console.log('I have been clicked!!');
+  toggleDiariesDetails();
+}
+
+const toggleDiariesDetails = () => {
+  const element = document.getElementsByClassName('hide-details');
+  element[1].classList.toggle('show-details');
+}
+
+// screen 3: word nerds
+const nerdsProject = document.querySelector('.word-nerds-screen');
+
+nerdsProject.addEventListener('click', handleNerdsClick);
+
+function handleNerdsClick() {
+  console.log('I have been clicked!!');
+  toggleNerdsDetails();
+}
+
+const toggleNerdsDetails = () => {
+  const element = document.getElementsByClassName('hide-details');
+  element[2].classList.toggle('show-details');
+}
+
+// screen 4: sanctuary helpers
+const sanctuaryProject = document.querySelector('.sanctuary-screen');
+
+sanctuaryProject.addEventListener('click', handleSanctuaryClick);
+
+function handleSanctuaryClick() {
+  console.log('I have been clicked!!');
+  toggleSanctuaryDetails();
+}
+
+const toggleSanctuaryDetails = () => {
+  const element = document.getElementsByClassName('hide-details');
+  element[3].classList.toggle('show-details');
+}
